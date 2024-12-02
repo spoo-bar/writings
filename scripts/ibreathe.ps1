fbc32776a0a1de9d4fa3ec7920c99208f0d5888c
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2024-12-02 15:50:36 +0530

    file paths for linux compatibility

--- scripts/ibreathe.ps1
+++ scripts/ibreathe.ps1
@@ -1,4 +1,4 @@
-$FILEPATH=".\_posts\2024-04-09-i-breathe.md"
+$FILEPATH="./_posts/2024-04-09-i-breathe.md"
 $APIKEY="$env:APIKEY"
 $BTCHEIGHT=Invoke-WebRequest -Uri "https://rest.cryptoapis.io/blockchain-data/bitcoin/mainnet/blocks/last/1" -Headers @{"Content-Type"="application/json"; "X-API-Key"="$APIKEY"} -Method Get | ConvertFrom-Json | Select-Object -ExpandProperty data | Select-Object -ExpandProperty items | Select-Object -ExpandProperty height 
 $IBREATHECONTENT = [System.IO.File]::ReadAllText($FILEPATH).Replace("[BLOCKHEIGHT]","$BTCHEIGHT")
