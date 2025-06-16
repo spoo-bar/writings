525d87128dcf6b733e365fbe56ac97ddd05ec799
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2025-06-16 16:34:30 +0100

    refactor: improve Jupyter notebook listing and conversion process

--- scripts/jupyter.ps1
+++ scripts/jupyter.ps1
@@ -9,6 +9,9 @@ $env:PATH += ";$env:USERPROFILE\.local\bin"
 pipx install jupyter_contrib_nbextensions --include-deps
 
 # Recursively list all .ipynb files inside the _posts directory
-Get-ChildItem -Path "./_posts" -Recurse -Filter "*.ipynb" | Select-Object -ExpandProperty
+$notebooks = Get-ChildItem -Path "./_posts" -Recurse -Filter "*.ipynb" | Select-Object -ExpandProperty FullName
 
-python notebooks/jupyter_to_jekyll.py ./_posts/2025-06-10-rareskills-zk-bootcamp-pre-course-practise/2025-06-10-rareskills-zk-bootcamp-pre-course-practise.ipynb
+# Convert each Jupyter notebook to Jekyll markdown using the provided Python script
+foreach ($notebook in $notebooks) {
+    python notebooks/jupyter_to_jekyll.py $notebook
+}
